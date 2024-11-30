d00ca8fa91efa3830a5d828306779a8332284d5a
mohammedmoqbel <mohammedali.moqbel@ozu.edu.tr>
2024-11-30 23:08:57 +0300

    de

--- app/src/main/java/com/example/ourchatapp/MainActivity.kt
+++ app/src/main/java/com/example/ourchatapp/MainActivity.kt
@@ -14,6 +14,8 @@ class MainActivity : AppCompatActivity() {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+
+
             insets
         }
     }
