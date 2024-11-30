f60d741a07dcff1ad6f7916a9f8fcd209a68b2ee
elbakri-elbashir-ozu <147049682+MohamedElbashir10@users.noreply.github.com>
2024-12-01 00:41:13 +0300

    SignIn+

--- app/src/main/java/com/example/ourchatapp/mvvm/ChatAppViewModel.kt
+++ app/src/main/java/com/example/ourchatapp/mvvm/ChatAppViewModel.kt
@@ -1,4 +1,11 @@
 package com.example.ourchatapp.mvvm
 
-class ChatAppViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.MutableLiveData
+
+class ChatAppViewModel: ViewModel() {
+
+    val name = MutableLiveData<String>()
+    val imageUrl =  MutableLiveData<String>()
+    val message =  MutableLiveData<String>()
 }
