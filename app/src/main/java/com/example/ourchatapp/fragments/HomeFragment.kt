85c39d47923502f8db1e729710222ff2d51afa5c
mohammedmoqbel <mohammedali.moqbel@ozu.edu.tr>
2024-12-01 17:35:31 +0300

    qa

--- app/src/main/java/com/example/ourchatapp/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/ourchatapp/fragments/HomeFragment.kt
@@ -1,22 +1,29 @@
 package com.example.ourchatapp.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.ourchatapp.R
+import com.example.ourchatapp.SignInActivity
+import com.example.ourchatapp.adapter.OnItemClickListener
 import com.example.ourchatapp.adapter.UserAdapter
 import com.example.ourchatapp.databinding.FragmentHomeBinding
+import com.example.ourchatapp.model.Users
 import com.example.ourchatapp.mvvm.ChatAppViewModel
 import com.google.firebase.auth.FirebaseAuth
+import de.hdodenhof.circleimageview.CircleImageView
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment() , OnItemClickListener {
 
     lateinit var rvUsers: RecyclerView
     lateinit var useradapter: UserAdapter
@@ -24,6 +31,9 @@ class HomeFragment : Fragment() {
     lateinit var homdebinding: FragmentHomeBinding
 
     lateinit var fbauth: FirebaseAuth
+
+    lateinit var toolbar: Toolbar
+    lateinit var circleImageView: CircleImageView
 
 
     override fun onCreateView(
@@ -41,6 +51,11 @@ class HomeFragment : Fragment() {
 
         fbauth = FirebaseAuth.getInstance()
 
+        toolbar = view.findViewById(R.id.toolbarMain)
+        circleImageView = view.findViewById(R.id.tlImage)
+
+        homdebinding.lifecycleOwner =viewLifecycleOwner
+
         userViewModel = ViewModelProvider(this).get(ChatAppViewModel::class.java)
         useradapter = UserAdapter()
         rvUsers = view.findViewById(R.id.rvUsers)
@@ -52,16 +67,24 @@ class HomeFragment : Fragment() {
 
         userViewModel.getUsers().observe(viewLifecycleOwner, Observer {
            useradapter.setList(it)
+            useradapter.setOnClickListener(this)
             rvUsers.adapter= useradapter
         })
 
 
         homdebinding.logOut.setOnClickListener {
             fbauth.signOut()
-
-
+            startActivity(Intent(requireContext(), SignInActivity::class.java))
 
         }
 
+        userViewModel.imageUrl.observe(viewLifecycleOwner,Observer{
+            Glide.with(requireContext()).load(it).into(circleImageView)
+        })
+
+    }
+
+    override fun onUserSelected(position: Int, users: Users) {
+        TODO("Not yet implemented")
     }
 }
--- app/src/main/java/com/example/ourchatapp/mvvm/ChatAppViewModel.kt
+++ app/src/main/java/com/example/ourchatapp/mvvm/ChatAppViewModel.kt
@@ -3,10 +3,16 @@ package com.example.ourchatapp.mvvm
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.ourchatapp.MyApplication
+import com.example.ourchatapp.SharedPrefs
+import com.example.ourchatapp.Utils
 import com.example.ourchatapp.model.Users
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class ChatAppViewModel: ViewModel() {
+class  ChatAppViewModel: ViewModel() {
 
     val message = MutableLiveData<String>()
     val firestore = FirebaseFirestore.getInstance()
@@ -21,10 +27,34 @@ class ChatAppViewModel: ViewModel() {
 
 
 
+    init {
+        getCurrentUser()
+    }
+
+
     fun getUsers(): LiveData<List<Users>> {
         return usersRepo.getUsers()
     }
 
+    fun getCurrentUser()= viewModelScope.launch(Dispatchers.IO){
+        val context= MyApplication.instance.applicationContext
+
+        firestore.collection("Users").document(Utils.getUiLoggedIn()).addSnapshotListener{value,error->
+
+            if(value!!.exists()&& value!=null){
+                val users = value.toObject(Users::class.java)
+                name.value = users?.username!!
+                imageUrl.value = users.imageUrl!!
+
+
+                val mySharedPrefs = SharedPrefs(context)
+
+                mySharedPrefs.setValue("username", users.username)
+            }
+        }
+
+
+    }
 
 
 }
